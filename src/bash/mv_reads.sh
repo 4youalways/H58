14dd9a79c11fe95890d02859221579c7599cbc14
Allan Zuza <89768380+4youalways@users.noreply.github.com>
2025-02-08 12:58:34 +0200

    Update mv_reads.sh

--- src/bash/mv_reads.sh
+++ src/bash/mv_reads.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 samples=(1017142 BKQU3X A58390 BKQT8S)
-sample_dir="/Users/zuza/repos/H58/data"
-new_dir="/Users/zuza/repos/H58/data/ont"
+sample_dir="./data"
+new_dir="./data/ont"
 
 for sample in ${samples[@]}; do
     echo "Processing sample $sample"
     reads="${sample_dir}/${sample}"
 
     for read in  ${reads}_*/fastq*; do
-    new_name="/Users/zuza/repos/H58/data/ont/${sample}_0.fastq.gz"
+    new_name="./data/ont/${sample}_0.fastq.gz"
     cp $read $new_name
     echo "Copied $read to $new_name"
     echo
